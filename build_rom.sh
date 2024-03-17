b72ac13f848764d77587b81a6423cb09fcb9b8eb
eldon <121272988+eldonita@users.noreply.github.com>
2024-03-17 19:30:54 +0000

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,18 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/DerpFest-11/manifest.git -b 11 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/pocox3pro/Local-Manifests.git --depth 1 -b master .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+repo init -u https://github.com/SuperiorExtended/manifest -b UDC --git-lfs
+repo sync --force-sync
+git clone https://github.com/LineageOS/android_device_xiaomi_lancelot -b lineage-21 device/xiaomi/lancelot
+git clone https://github.com/LineageOS/android_device_xiaomi_mt6768-common -b lineage-21 device/xiaomi/mt6768-common
+git clone https://github.com/LineageOS/android_kernel_xiaomi_mt6768 -b lineage-21 kernel/xiaomi/mt6768
+git clone https://github.com/acex88/vendor_aeonax_ANXCamera -b v202 vendor/aeonax/ANXCamera
+git clone https://github.com/LineageOS/android_hardware_xiaomi -b lineage-21 hardware/xiaomi
+git clone https://github.com/LineageOS/android_hardware_mediatek -b lineage-20 hardware/mediatek
 
 # build rom
 source build/envsetup.sh
-lunch derp_vayu-user
+lunch lacelot user
 export TZ=Asia/Dhaka #put before last build command
-mka derp
+mka lancelot user
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
